5f22878c51f24c4e86a23025a50c56a463c1391b
raojiajun <471412450@q.com>
2022-12-08 15:22:18 +0800

    1. 使用 html-webpack-plugin 自动生成 index.html 2. 在 index.html 中自动注入打包产物

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    filename: 'bundle.js',
+    filename: '[hash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, './src/index.html'),
+  })],
 
-  }
 }
